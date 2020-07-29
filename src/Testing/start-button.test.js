9196abac9a91b080a846f3ed5c99756c78d70baf
Mako Williams <57023467+makowilliams@users.noreply.github.com>
2020-07-28 18:02:58 -0700

    added testing for components

---
+++ src/Testing/start-button.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import StartButton from '../Components/start-button';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+    <BrowserRouter>
+    <StartButton />
+    </BrowserRouter>, 
+    div
+    );
+    //ReactDOM.unmountComponentAtNode(div);
+});
