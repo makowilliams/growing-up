dafad8675ffec2db51aad7e67f536e1fc6ec304b
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-30 15:47:51 -0400

    POST sleep/eating data working
    will need to change out bearer token - hard coded currently
    change out when login complete and have tokenService file

--- src/Components/stop-button.js
+++ src/Components/stop-button.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import config  from '../config';
+//import TokenService from '../services/token-service';
 
 export default class StopButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            error: null
+        };
     }
 
     submitFeeding(e) {
         e.preventDefault();
+        this.setState({ error: null });
         let { duration, date, food_type, side_fed } = this.props.context;
         let feedingData = {
-            type: this.props.type,
+            type: 'eating',
             duration: duration,
             date: date,
             food_type: food_type,
             side_fed: side_fed
         };
         console.log(feedingData);
+        this.postData(feedingData);
     }
 
     submitSleep(e) {
         e.preventDefault();
+        this.setState({ error: null });
         let { duration, date, sleep_type, sleep_category } = this.props.context;
         let feedingData = {
             type: this.props.type,
@@ -31,9 +38,42 @@ export default class StopButton extends React.Component {
             sleep_category: sleep_category
         };
         console.log(feedingData);
+        this.postData(feedingData);
     }
 
+    postData = (data) => {
+        //still need to get childId somehow
+        let childId = 1
+        return fetch(`${config.API_ENDPOINT}/${data.type}/all/${childId}`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTYxMzc1NDgsInN1YiI6InRlc3RfdyJ9.SGR7XQl2Y_1gX6ZFzuZYyANAUsblMGyz0cLXNkONhJM'
+                //add back in once there is a login function
+                //authorization: `bearer ${TokenService.getAuthToken()}`,
+            },
+            body: JSON.stringify(data)
+        }).then(res => {
+            if (!res.ok) {
+                return res.json().then(error => {
+                    throw error;
+                });
+            }
+            return res.json();
+        })
+        .then(returnData => {
+            console.log('success', returnData)
+            //add data to client - implement the add Session function
+            // this.context.addSession();
+            // this.props.history.goBack();
+        })
+        .catch(error => {
+            this.setState({ error: 'Sorry, the session did not add. Please try again later.' });
+        });
+    };
+
     render() {
+        const { error } = this.state;
         return (
             <>
                 {this.props.type === 'feeding' ? (
@@ -53,6 +93,7 @@ export default class StopButton extends React.Component {
                         Submit Sleep
                     </Link>
                 )}
+                <div role="alert">{error && <p className="error">{error}</p>}</div>
             </>
         );
     }
