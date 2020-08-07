ed0d844a11880f7fc016b6e7adcbf15185936217
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-06 22:12:49 -0400

    moved app test, removed debugger

--- src/auth-api-service.js
+++ src/auth-api-service.js
@@ -2,20 +2,16 @@ import config from './config';
 
 const AuthApiService = {
     postLogin(credentials) {
-      //debugger
         return fetch(`${config.API_ENDPOINT}/auth/login`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-      })
-        .then(res =>
-          (!res.ok)
-            ? res.json().then(e => Promise.reject(e))
-            : res.json()
-        )
-    },
-  }
-  
-  export default AuthApiService
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        }).then((res) =>
+            !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+        );
+    }
+};
+
+export default AuthApiService;
