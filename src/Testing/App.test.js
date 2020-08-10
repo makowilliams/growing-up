7db1e128ed15c16507c868bc0c2cfe9289a12083
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-10 12:17:00 -0400

    formatting on test files

--- src/Testing/App.test.js
+++ src/Testing/App.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import App from '../App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/The App That Grows With Your Family/i);
-  expect(linkElement).toBeInTheDocument();
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/The App That Grows With Your Family/i);
+    expect(linkElement).toBeInTheDocument();
 });
