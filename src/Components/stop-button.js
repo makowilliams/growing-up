f5fa499f1d140d3d95f527011cda5d011e5ba0de
Mac Hale <machaledesign@gmail.com>
2020-08-17 20:26:37 -0600

    making pull req

--- src/Components/stop-button.js
+++ src/Components/stop-button.js
@@ -36,6 +36,7 @@ export default class StopButton extends React.Component {
                 side_fed: side_fed
             };
             this.postData(sessionData);
+            // initialize the context for duration and food type
         }
     }
 
